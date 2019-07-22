638043d98f8bc5dbc69a16b5f919e18d6c1dec8e
David Holbrook <david@davidholbrook.info>
2019-07-21 19:40:23 -0500

    removed excess images and added favicon
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#E84118`,
         theme_color: `#E84118`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/davidholbrook-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
