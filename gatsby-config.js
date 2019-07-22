3d4a7573cc289b7ecdc46d984b6fe37bb9697958
David Holbrook <david@davidholbrook.info>
2019-07-21 19:24:04 -0500

    setup basic configuration options
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `David Holbrook`,
+    description: `Web developer for Wihatools USA, also designs things.`,
+    author: `@davidholbrook`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,11 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `David Holbrook`,
+        short_name: `davidholbrook`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#E84118`,
+        theme_color: `#E84118`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
