2686e235906e245fce3ccfd4d2057c42100c5dd9
David Holbrook <david@davidholbrook.info>
2019-12-01 16:29:46 -0600

    added first blog post (draft)
    Signed-off-by: David Holbrook <david@davidholbrook.info>

---
+++ src/subcomponents/blog.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Blog = () => {
+  return (
+    <div>
+      <h3>Blog Title</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quae odio
+        facilis laboriosam veniam ad facere ipsa! Et suscipit voluptate quidem
+        veritatis rem nisi blanditiis nobis. Autem obcaecati ullam expedita!
+      </p>
+      <a href="#">link to post</a>
+      {/* simulate a gap on a page */}
+      <br />
+      <br />
+    </div>
+  )
+}
+
+export default Blog
