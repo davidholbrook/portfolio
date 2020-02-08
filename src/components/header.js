cd3fe96e203a400f6e233d16e2f4bd8d596878aa
David Holbrook <david@davidholbrook.info>
2020-02-08 14:39:43 -0600

    changed the position of the header
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- src/components/header.js
+++ src/components/header.js
@@ -6,60 +6,22 @@ import styled from "styled-components"
 import Icon from "../images/icons"
 
 const Header = () => (
-  <header>
-    <Heading>
-      {window.matchMedia("(prefers-color-scheme: dark)").matches ? (
-        <Icon name="logo--dark" />
-      ) : (
-        <Icon name="logo--light" />
-      )}
-      <span>
-        <h2>David Holbrook</h2>
-        <h3>Web Developer for Wiha Tools USA</h3>
-      </span>
-    </Heading>
-  </header>
+  <HeadBG>
+    {window.matchMedia("(prefers-color-scheme: dark)").matches ? (
+      <Icon name="logo--light" />
+    ) : (
+      <Icon name="logo--dark" />
+    )}
+  </HeadBG>
 )
 
 export default Header
 
-const Heading = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 2rem 2rem 3rem 2rem;
-
-  @media (prefers-color-scheme: dark) {
-    color: #cccccc;
-  }
+const HeadBG = styled.header`
+  background: #2d3436;
+  height: 100vh;
 
   svg {
-    margin: -0.5rem 1rem 0 0;
-  }
-
-  h2 {
-    font-family: "Nobile", sans-serif;
-    font-size: 5rem;
-
-    @media (max-width: 600px) {
-      text-transform: uppercase;
-      font-size: 25vw;
-      text-align: center;
-    }
-  }
-  h3 {
-    font-family: "Work Sans", sans-serif;
-    font-weight: 300;
-    line-height: 1.5;
-    text-transform: uppercase;
-    margin: -2rem 0 0 0;
-    // evil I know
-    letter-spacing: 2.5px;
-
-    @media (max-width: 600px) {
-      font-size: 9vw;
-      line-height: 3.5rem;
-      text-align: center;
-    }
+    margin: 1rem auto;
   }
 `
--- src/components/layout.js
+++ src/components/layout.js
@@ -25,41 +25,41 @@ const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyle />
-      <MainContent>
+      <Grid>
         <Header />
-        {children}
-        <Footer>
-          <p>
-            <a href="https://creativecommons.org/licenses/by-sa/4.0/">
-              Attribution-ShareAlike 4.0 International
-            </a>{" "}
-            license | Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </p>
-        </Footer>
-      </MainContent>
+        <MainContent>
+          {children}
+          <Footer>
+            <p>
+              <a href="https://creativecommons.org/licenses/by-sa/4.0/">
+                Attribution-ShareAlike 4.0 International
+              </a>{" "}
+              license | Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </p>
+          </Footer>
+        </MainContent>
+      </Grid>
     </>
   )
 }
 
 export default Layout
 
-const MainContent = styled.main`
-  width: 90vw;
-  margin: 0 auto;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 8rem 1fr;
+`
 
-  @media (min-width: 900px) {
-    margin: 2rem 15vw;
-    width: 60vw;
-  }
+const MainContent = styled.main`
+  width: 50rem;
+  margin-left: 1rem;
 `
 
 const Footer = styled.footer`
   font-family: "Work Sans", sans-serif;
   line-height: 1.8rem;
-
-  text-align: center;
 
   margin: 0;
 
