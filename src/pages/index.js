f5dcff727d42f7f379436aa1b9e570c3e87bef2a
David Holbrook <david@davidholbrook.info>
2020-05-17 20:04:13 -0500

    header reset
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,24 +6,44 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import Background from "../images/background.png"
+
 const IndexPage = () => (
   <Layout>
-    <SEO title="Welcome" />
-    <h1>David Holbrook</h1>
-    <div>
-      <p>
-        I am a Developer working for{" "}
-        <a
-          href="https://www.wihatools.com/about-wiha-tools"
-          alt="Wiha Tools USA, website"
-        >
-          Wiha Tools USA
-        </a>
-        . I love Comic Books, Video Games and Star Wars, but most of all I have
-        a passion for web development and making cool useful things..
-      </p>
-    </div>
+    <SEO title="Minneapolis Developer" />
+    <Intro>
+      <h1>David Holbrook</h1>
+      <h2>Generating Websites. Star Wars Trivia, and Comic Book Love.</h2>
+    </Intro>
   </Layout>
 )
 
 export default IndexPage
+
+const Intro = styled.div`
+  color: #999;
+
+  background: #333 url('${Background}') no-repeat;
+  background-size: cover;
+
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  h1 {
+    margin-top: 0;
+    margin-bottom: 1rem;
+
+    text-transform: uppercase;
+
+    font-size: 4rem;
+  }
+
+  h2{
+    font-weight: 200px;
+  }
+`
