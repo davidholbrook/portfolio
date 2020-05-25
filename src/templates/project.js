6ac3401d8a3266f51b38cecde66ca4bb5e3a4608
David Holbrook <david@davidholbrook.info>
2020-05-24 20:55:31 -0500

    added layout components to layout folder
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../layout/layout"
+import SEO from "../layout/seo"
 import styled from "styled-components"
 
 export default ({ data }) => {
