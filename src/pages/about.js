0dff7d30e22e293bd9116848480c807958d61fec
David Holbrook <david@davidholbrook.info>
2020-06-14 17:11:09 -0500

    swaped DetailBox
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,15 +21,16 @@ const About = () => {
           <span>
             <h2>About David Holbrook</h2>
             <p>
-              Hey, I'm David, an e-commerce developer for Wiha Tools USA. I have
-              been working on websites for the last 15 years and have a degree
-              in Entrepreneurship Studies. I love to blog about development, the
-              state of e-commerce, video games, comic books, and sometimes even
-              Star Wars.
+              Hey, I'm David, an e-commerce developer for{" "}
+              <a href="https://wihatools.com">Wiha Tools USA</a>. I have been
+              working on websites for the last 15 years and have a degree in
+              Entrepreneurship Studies. I love to blog about development, the
+              state of e-commerce, video games, comic books, and sometimes Star
+              Wars.
             </p>
           </span>
         </AboutBox>
-        <Title>Some of my Favorite Things</Title>
+        <Title>Some Fun Facts about Me</Title>
         <BoxWrapper>
           <DetailBox>
             <h4>Favorite Devtools</h4>
@@ -42,23 +43,27 @@ const About = () => {
             </ul>
           </DetailBox>
           <DetailBox>
-            <h4>Most Experenced Devtools</h4>
-            <ul>
-              <li>Node</li>
-              <li>React</li>
-              <li>Magento 2</li>
-              <li>Session Box</li>
-              <li>VS Code</li>
-            </ul>
-          </DetailBox>
-          <DetailBox>
-            <h4>Favorite Fandom</h4>
+            <h4>Favorite Fandoms</h4>
             <ul>
               <li>Star Wars</li>
               <li>Nintendo</li>
               <li>Apple</li>
               <li>Halo</li>
               <li>Assassin’s Creed</li>
+            </ul>
+          </DetailBox>
+          <DetailBox>
+            <h4>Statistics</h4>
+            <ul>
+              <li>
+                <strong>Years of Coding:</strong> 10
+              </li>
+              <li>
+                <strong>Number of Programing Languages Used:</strong> 5
+              </li>
+              <li>
+                <strong>Number of Coding Lessons:</strong> Way to many
+              </li>
             </ul>
           </DetailBox>
         </BoxWrapper>
@@ -96,9 +101,11 @@ const DetailBox = styled.div`
 
   display: grid;
   justify-content: center;
+  align-content: start;
 
   h4 {
     margin-bottom: 0;
+    text-align: center;
   }
   ul {
     list-style: none;
