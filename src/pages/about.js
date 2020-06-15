bcac92331754f2d6fff658a23b153f275273526f
David Holbrook <david@davidholbrook.info>
2020-06-14 19:18:07 -0500

    read more toggle
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
+import { useSpring, animated } from "react-spring"
+import useMeasure from "../utils/useMeasure"
 
 import Layout from "../layout/layout"
 import SEO from "../layout/seo"
@@ -10,6 +12,17 @@ import Navagation from "../components/navagation"
 import Avatar from "../images/avatar.jpg"
 
 const About = () => {
+  const [toggle, setToggle] = useState(true)
+  const [bind, { height, top }] = useMeasure()
+
+  const readMore = useSpring({
+    overflow: "hidden",
+    height: toggle ? 0 : height,
+    opacity: toggle ? 0 : 1,
+    transform: toggle ? "translateY(0)" : "translateY(-2rem)",
+    config: { duration: 320 },
+  })
+
   return (
     <Layout>
       <SEO title="About David" />
@@ -28,6 +41,29 @@ const About = () => {
               state of e-commerce, video games, comic books, and sometimes Star
               Wars.
             </p>
+            <animated.div style={readMore}>
+              <div {...bind} style={{ padding: "1rem 0 0 0" }}>
+                <p>
+                  As a developer, I have worked with many different kinds of
+                  development languages and frameworks. Starting with WordPress
+                  and ending with Gatsby I have worked with PHP, Javascript
+                  (including the new ECMAScript features), node, and even
+                  dabbled some in the future of development, such as GraphQL and
+                  the new headless commerce concepts.
+                </p>
+                <p>
+                  I have more than that to share in my blog (called thoughts, as
+                  they are my thoughts about the current topics on the web) and
+                  you can see how I implement these concepts both on my Github
+                  and my side projects, which are in-depth looks into
+                  side-projects I am currently working on.
+                </p>
+                <br />
+              </div>
+            </animated.div>
+            <Button onClick={() => setToggle(!toggle)}>
+              {toggle ? "Learn More" : "Less Information"}
+            </Button>
           </span>
         </AboutBox>
         <Title>Some Fun Facts about Me</Title>
@@ -87,6 +123,19 @@ const AboutBox = styled.div`
   a {
     color: #ffffff;
   }
+`
+
+const Button = styled.button`
+  background: #ecf0f1;
+  color: #333333;
+
+  text-transform: uppercase;
+  letter-spacing: 1px;
+
+  padding: 0.5rem 0.5rem 0.3rem 0.5rem;
+  border: none;
+
+  border-radius: 0.2rem;
 `
 
 const Img = styled.img`
