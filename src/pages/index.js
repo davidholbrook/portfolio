998aa33d254e5757460b4e30c6edc947c003a4b4
David Holbrook <david@davidholbrook.info>
2021-08-21 13:00:08 -0500

    Layout file renamed
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import styled from "styled-components"
-import Layout from "../Layout/main"
+import Layout from "../layout/layout"
 import Navagation from "../Components/navagation"
 import bgImg from "../images/headerbg.jpg"
 
