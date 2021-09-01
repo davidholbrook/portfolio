0fb357d30d189a857efc72c0f80a5bc5d545654f
David Holbrook <david@davidholbrook.info>
2021-08-31 21:58:34 -0500

    Inital design finished
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- src/templates/projects.js
+++ src/templates/projects.js
@@ -1,36 +1,62 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../layout/layout"
-import SEO from "../layout/seo"
-import styled from "styled-components"
+import Seo from "../layout/seo"
+import styled, { createGlobalStyle } from "styled-components"
 
-export default ({ data }) => {
+import Navagation from "../components/navagation"
+import "../styles/typography.css"
+
+const Project = ({ data }) => {
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
 
-  const Post = styled.div`
+  const GlobalStyle = createGlobalStyle`
+  body {
+    background: ${frontmatter.color};
+  }
+`
+
+  const Project = styled.div`
     a {
       color: ${frontmatter.color};
       border-color: ${frontmatter.color};
     }
   `
 
-  const Title = styled.h2`
-    color: ${frontmatter.color};
+  const Company = styled.h2`
+    color: #ffffff;
+    @media (min-width: 1024px) {
+      color: ${frontmatter.color};
+    }
   `
   return (
     <>
-      <SEO title={frontmatter.title} />
+      <GlobalStyle />
+      <Seo title={frontmatter.title} />
       <Layout>
         <div className="container mx-auto">
-          <Title class="text=3xl font-bold">{frontmatter.title}</Title>
-          <Post dangerouslySetInnerHTML={{ __html: html }} />
+          <Navagation />
+          <div className="col-span-1 w-full lg:w-64 lg:fixed lg:bg-white px-10 lg:px-4 py-3 mt-0 rounded-lg">
+            <Company className="text-5xl lg:text-3xl font-bold mt-3">
+              {frontmatter.title}
+            </Company>
+            <p className="text-sm text-white lg:text-black">{frontmatter.sd}</p>
+            <p className="text-sm text-white lg:text-black">
+              <strong>Role: </strong>
+              {frontmatter.role}
+            </p>
+          </div>
+          <Project
+            dangerouslySetInnerHTML={{ __html: html }}
+            className="lg:ml-72 block bg-white px-10 py-4 mt-0 lg:mb-10 lg:rounded-lg lg:shadow-lg"
+          />
         </div>
       </Layout>
     </>
   )
 }
-
+export default Project
 export const pageQuery = graphql`
   query ($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
@@ -39,6 +65,9 @@ export const pageQuery = graphql`
         title
         path
         color
+        role
+        url
+        sd
       }
     }
   }
