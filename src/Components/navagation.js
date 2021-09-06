e8fe88ac3cc1d8bfe07fad33dd901541adf791a9
David Holbrook <david@davidholbrook.info>
2021-09-05 20:06:38 -0500

    Links changed to Gatsby tags and links added
    Links are currently non-working.
    
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- src/Components/navagation.js
+++ src/Components/navagation.js
@@ -1,26 +1,29 @@
 import React from "react"
 import Logo from "../images/logo_new_white.svg"
+import { Link } from "gatsby"
 
 export const navagation = () => {
   return (
     <div className="px-10 py-8 flex justify-between align-center">
-      <img src={Logo} alt="" width="100px" />
+      <Link to="/">
+        <img src={Logo} alt="" width="100px" />
+      </Link>
       <nav>
         <ul className="flex">
           <li className="ml-5">
-            <a href="#" className="text-2xl text-white uppercase">
+            <Link to="/about" className="text-2xl text-white uppercase">
               About
-            </a>
+            </Link>
           </li>
           <li className="ml-5">
-            <a href="#" className="text-2xl text-white uppercase">
+            <Link to="/work" className="text-2xl text-white uppercase">
               Work
-            </a>
+            </Link>
           </li>
           <li className="ml-5">
-            <a href="#" className="text-2xl text-white uppercase">
-              Blog
-            </a>
+            <Link to="/thoughts" className="text-2xl text-white uppercase">
+              Thoughts
+            </Link>
           </li>
         </ul>
       </nav>
