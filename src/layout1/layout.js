009b6d95875b54592d79ec4e3ac091727ab92bce
David Holbrook <david@davidholbrook.info>
2021-10-18 02:52:02 -0500

    Incorrect directory casing
    Signed-off-by: David Holbrook <david@davidholbrook.info>

---
+++ src/layout1/layout.js
@@ -0,0 +1,29 @@
+import React from "react"
+import "./styles.css"
+export default function Layout({ children }) {
+  return (
+    <>
+      {children}
+      <footer className="text-center bg-black text-white text-xl p-5">
+        Protected under a{" "}
+        <a
+          href="https://creativecommons.org/licenses/by-sa/4.0/"
+          className="text-gray-500 underline"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Attribution-ShareAlike 4.0 International
+        </a>{" "}
+        license | Built with{" "}
+        <a
+          href="https://www.gatsbyjs.org"
+          className="text-gray-500 underline"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Gatsby
+        </a>
+      </footer>
+    </>
+  )
+}
