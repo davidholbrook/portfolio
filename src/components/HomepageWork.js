6d310ea3028e50c0a7b8386cbc6a7bc6a5063fc6
David Holbrook <david@davidholbrook.info>
2021-12-06 18:03:33 -0600

    homepage work section redesigned
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- src/components/HomepageWork.js
+++ src/components/HomepageWork.js
@@ -35,31 +35,38 @@ const HomepageWork = () => {
       <h2 className="text-3xl lg:text-xl container mx-auto mt-5 uppercase text-center lg:text-left lg:border-b-2 lg:border-black standardfont lg:font-normal">
         My Work
       </h2>
-      <div className="container mx-auto m-10 p-5 lg:p-0 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center ">
+      <div className="container mx-auto m-10 p-5 lg:p-0 lg:grid lg:grid-cols-2 lg:gap-2">
         {data.allMarkdownRemark.edges.map(
           ({ node }) =>
             node.frontmatter.template === "project" && (
-              <div className="mb-20 lg:mb-0 md:grid md:grid-cols-2 md:gap-8">
+              <div
+                className="mb-20 lg:mb-0 relative"
+                style={{ background: node.frontmatter.color }}
+              >
                 <Link to={node.frontmatter.path}>
                   <GatsbyImage
                     image={
                       node.frontmatter.frontImage.childImageSharp
                         .gatsbyImageData
                     }
+                    className="w-full h-full opacity-50"
                   />
                 </Link>
-                <span>
+                <div
+                  className="absolute bottom-5 left-4 w-11/12 mx-2 p-5 "
+                  style={{ background: node.frontmatter.color }}
+                >
                   <h2>{node.frontmatter.title}</h2>
                   <p className="my-0 leading-loose text-gray-700 dark:text-gray-300">
                     {node.frontmatter.sd}
                   </p>
                   <Link
                     to={node.frontmatter.path}
-                    className="inline-block border-2 border-gray-700 dark:border-gray-300 text-gray-700 dark:text-gray-300 p-2 mt-2 uppercase"
+                    className="inline-block border-2 border-gray-700 dark:border-gray-300 text-gray-700 dark:text-gray-300 p-2 mt-2 uppercase hover:bg-white hover:text-black hover:border-white"
                   >
                     Read More
                   </Link>
-                </span>
+                </div>
               </div>
             )
         )}
